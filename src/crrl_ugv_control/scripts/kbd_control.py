14af7433ada346d1a8aa58513f41b0733c295e34
Halil Utku Unlu <h.utkuunlu@gmail.com>
2020-05-29 12:07:38 +0300

    Keyboard controls improved for better response time

--- src/crrl_ugv_control/scripts/kbd_control.py
+++ src/crrl_ugv_control/scripts/kbd_control.py
@@ -1,12 +1,21 @@
 import rospy
 from geometry_msgs.msg import Twist
-import getch
+import curses
 
 def send_vel():
-    rospy.init_node('cmd_vel', anonymous=True)
 
+    # Initialize terminal
+    stdscr = curses.initscr()
+    curses.noecho()
+    stdscr.nodelay(1)
+    stdscr.addstr(0, 0, "UGV Keyboard Control")
+
+    # Initialize ROS topic and message
+    rospy.init_node('cmd_vel', anonymous=True)
     vel_publisher = rospy.Publisher('cmd_vel', Twist, queue_size=10)
-    
+    rate = rospy.Rate(10)
+
+    # Control parameters
     linear_vel = 0.0
     angular_vel = 0.0
 
@@ -15,31 +24,47 @@ def send_vel():
 
     linear_increment = 0.2
     angular_increment = 0.2
-    
+
     while not rospy.is_shutdown():
 
-        key = getch.getch()
+        # Read character if there is any
+        key = stdscr.getch()
 
-        if key == 'a':
+        # Terminate
+        if key == ord('q'):
+            break
+        
+        # Control updates wherever necessary
+        if key == ord('a'):
             angular_vel = min(angular_cap, angular_vel + angular_increment)
-        elif key == 'd':
+        elif key == ord('d'):
             angular_vel = max(-angular_cap, angular_vel - angular_increment)
-        elif key == 'w':
+        elif key == ord('w'):
             linear_vel = min(linear_cap, linear_vel + linear_increment)
-        elif key == 's':
+        elif key == ord('s'):
             linear_vel = max(-linear_cap, linear_vel - linear_increment)
+        elif key == ord(' '):
+            linear_vel = 0.0
+            angular_vel = 0.0
 
-        print("Linear: %.1f, Angular: %.1f" % (linear_vel, angular_vel))
+        # Write to screen
+        stdscr.clrtoeol()
+        stdscr.refresh()
+        stdscr.addstr(1, 0, "L: %.1f, A: %.1f" % (linear_vel, angular_vel))
             
+        # Send message
         vel_msg = Twist()
         vel_msg.linear.x = linear_vel
         vel_msg.angular.z = angular_vel
-
         vel_publisher.publish(vel_msg)
+
+        # Ensure correct rate
+        rate.sleep()
         
-            
 if __name__ == '__main__':
     try:
         send_vel()
     except rospy.ROSInterruptException:
         pass
+    finally:
+        curses.endwin()
